db4df227bd501b3421b8097fa432a5e3a332d708
admin <arnav.jain@dotsqures.com>
2025-08-22 14:53:02 +0530

    revert code

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: dd5uzhf3n,
-  api_key: 322945873875984,
-  api_secret: AwJ63btEPkP4XIeBm6DoIL3bQ58,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
