8b2c736fddf927ec6bf3f432e0e486994a48ea49
admin <deepak.k@dotsquares.com>
2025-10-08 11:14:06 +0530

    add tls

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,11 +6,14 @@ const sendEmail = async ({ to, subject, text }) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
-      port: parseInt(process.env.SMTP_PORT),
-      secure: process.env.SMTP_SECURE === 'true',
+      port: parseInt(process.env.SMTP_PORT, 10),
+      secure: process.env.SMTP_SECURE === 'true', // false for 587
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false, // add this for live hosting SSL issues
       },
     });
 
@@ -29,5 +32,3 @@ const sendEmail = async ({ to, subject, text }) => {
 };
 
 module.exports = sendEmail;
-
-
